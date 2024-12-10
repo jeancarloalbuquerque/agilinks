9b9ae48bd13ef033051eff9c0c2ec9800390f714
Jean Carlo Albuquerque <jeancarlo1584@gmail.com>
2024-12-10 15:39:38 -0400

    database connection moved

--- links/index.php
+++ links/index.php
@@ -1,6 +1,6 @@
 <?php
     require_once '../vendor/autoload.php'; 
-    require_once '../database.php';
+    require_once '../database/connection.php';
 
     $links = $mysqli->query('select * from links');
 ?>
