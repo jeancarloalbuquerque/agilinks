9eb4695437818a13652bb943619a3fea3f8035d3
Jean Carlo Albuquerque <jeancarlo1584@gmail.com>
2024-12-10 16:28:01 -0400

    navigation renamed

---
+++ resources/views/layouts/navigation/auth.php
@@ -0,0 +1,34 @@
+<header style="margin-bottom: 5rem;">
+    <div class="ui container">
+        <div class="ui secondary menu">
+            <a href="/" class="header item">
+                <i class="sitemap icon"></i>
+                Agilinks
+
+            </a>
+
+            <a href="#" class="item">
+                <i class="chart bar icon"></i>
+                Analytics
+            </a>
+            <a href="/admin/links" class="item">
+                <i class="linkify icon"></i>
+                Meus Links
+            </a>
+            <a href="#" class="item">
+                <i class="tags icon"></i>
+                Minhas Tags
+            </a>
+            
+
+            <div class="right floated menu">
+                <div class="item">
+                    <button class="ui negative button">
+                        <i class="sign out icon"></i>
+                        Logout
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
