09500736d760c006762011fd6d36715e117b7ee6
Jean Carlo Albuquerque <jeancarlo1584@gmail.com>
2024-12-10 17:07:15 -0400

    adding tags to links

--- admin/links/index.php
+++ admin/links/index.php
@@ -3,6 +3,13 @@
     require_once '../../database/connection.php';
 
     $links = $mysqli->query('select * from links');
+    
+    $tags = [
+        'Redes Socias',
+        'Portifólio',
+        'Produtos',
+        'Conteúdos',
+    ];
 ?>
 
 <!DOCTYPE html>
@@ -15,28 +22,44 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/fomantic-ui@2.9.3/dist/semantic.min.css">
     <script src="https://cdn.jsdelivr.net/npm/fomantic-ui@2.9.3/dist/semantic.min.js"></script>
 
-    <title>Document</title>
+    <title>Meus Links</title>
 </head>
 <body>
     <?php include_once '../../resources/views/layouts/navigation/auth.php' ?>
 
     <div class="ui text container">
-        <div class="ui divided link items">
-            <?php 
-                
 
+        <h1 class="ui header">
+            Meus Links
+        </h1>
+
+        <div class="ui divided relaxed link items">
+            <?php                 
                 foreach ($links as $link) {
                     echo 
                     "<div href='#' class='item'>
                         <div class='middle aligned content'>
-                            <a href='". $link['url'] ."' class='header'>". $link['title']. "</a>
+                            <a href='". $link['url'] ."' class='header'><i class='linkify icon'></i>". $link['title']. "</a>
                             <div class='description'>
                                 <p>". $link['description'] ."</p>
                             </div>
+                            
+                            <div class='extra'>";
+
+                                $link_tags = (array) array_rand($tags);
+                                
+                                foreach ($link_tags as $tag) {
+                                    echo "<span class='ui label'>$tags[$tag]</span>";
+                                }
+                            
+                            echo
+                            "</div>
                         </div>
                     </div>";
                 }
             ?>
+            
+
         </div>
     </div>
 </body>
