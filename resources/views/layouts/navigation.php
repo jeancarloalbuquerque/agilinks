a1fafcb23311cb98d3d3b4afe7b34fbab9168483
Jean Carlo Albuquerque <jeancarlo1584@gmail.com>
2024-12-10 15:29:08 -0400

    create navigation layout

---
+++ resources/views/layouts/navigation.php
@@ -0,0 +1,32 @@
+<header style="margin-bottom: 5rem;">
+    <div class="ui container">
+        <div class="ui secondary menu">
+            <a href="/agilinks" class="header item">
+                <i class="sitemap icon"></i>
+                Agilinks
+
+            </a>
+            <a href="#" class="item">
+                <i class="paint roller icon"></i>
+                Templeates
+            </a>
+            <a href="#" class="item">
+                <i class="dollar sign icon"></i>
+                Preços e Planos
+            </a>
+            <a href="#" class="item">
+                <i class="book icon"></i>
+                Aprender
+            </a>
+
+            <div class="right floated menu">
+                <div href="#" class="item">
+                    <a class="ui primary button">Login</a>
+                </div>
+                <div href="#" class="item">
+                    <a href="" class="ui button">Registrar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
