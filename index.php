15bc42bd3931c0667372c34ce43a1c397c237c66
Jean Carlo Albuquerque <jeancarlo1584@gmail.com>
2024-12-18 14:39:09 -0400

    update index styles

--- index.php
+++ index.php
@@ -11,48 +11,52 @@
     <title>Document</title>
 </head>
 <body>
-    <?php include_once 'resources/views/layouts/navigation.php' ?>
+    <?php include_once 'resources/views/layouts/navigation/guest.php' ?>
     
     <main>
-        <div class="ui text container">
+        <div class="ui container">
 
-            <h1>Ajude seus seguidores a descobrir tudo que você faz, com apenas um link</h1>
-            <a href="#" class="ui primary button">Começar agora</a>
+            <div class="ui center aligned basic segment">
+                <h1>Ajude seus seguidores a descobrir tudo que você faz, com apenas um link</h1>
+                <a href="./admin" class="ui primary button">Começar agora</a>
+            </div>
 
             <div class="ui hidden section divider"></div>
 
-            <h2>
-                <span>Usado por mais de 50M de </span>
-
-                <div class="ui text shape" id="users-shape">
-                    <div class="sides">
-                        <div class="active side"><span class="ui primary text">Músicos</span></div>
-                        <div class="side"><span class="ui primary text">Bandas</span></div>
-                        <div class="side"><span class="ui primary text">Escritores</span></div>
-                        <div class="side"><span class="ui primary text">Criadores</span></div>
-                        <div class="side"><span class="ui primary text">Influenciadores</span></div>
-                        <div class="side"><span class="ui primary text">Pequenos Negócios</span></div>
-                        <div class="side"><span class="ui primary text">Atletas</span></div>
-                        <div class="side"><span class="ui primary text">Streemers</span></div>
-                        <div class="side"><span class="ui primary text">Vloogers</span></div>
+            <div class="ui basic segment">
+                <h2>
+                    <span>Usado por mais de 50M de </span>
+                    <div class="ui text shape" id="users-shape">
+                        <div class="sides">
+                            <div class="active side"><span class="ui primary text">Músicos</span></div>
+                            <div class="side"><span class="ui primary text">Bandas</span></div>
+                            <div class="side"><span class="ui primary text">Escritores</span></div>
+                            <div class="side"><span class="ui primary text">Criadores</span></div>
+                            <div class="side"><span class="ui primary text">Influenciadores</span></div>
+                            <div class="side"><span class="ui primary text">Pequenos Negócios</span></div>
+                            <div class="side"><span class="ui primary text">Atletas</span></div>
+                            <div class="side"><span class="ui primary text">Streemers</span></div>
+                            <div class="side"><span class="ui primary text">Vloogers</span></div>
+                        </div>
                     </div>
-                </div>
-            </h2>
+                </h2>
+            </div>
 
             <div class="ui section hidden divider"></div>
 
-            <h2>Você nunca mais vai precisar mudar link da sua Bio</h2>
-            <p>
-                Em 2024 Agilinks resolveu o maior problema das redes sociais: ter apenas um link na sua bio.
-            </p>
-            <p>
-                Agilinks foi o primeiro plataforma de 'link na bio' criada para resolver esse problema 
-            </p>
-
-            <p>
-                Desde então Agilinks se tornou muito mais. Permitindo empresas e criadores a conseguir muito mais das suas redes socias, 
-                aumentando seus seguidores, recebendo pagamentos de forma descomplicada e devolvendo o controle de como seus conteúdos são descobertos.
-            </p>
+            <div class="ui basic segment">
+                <h2>Você nunca mais vai precisar mudar link da sua Bio</h2>
+                <p>
+                    Em 2024 Agilinks resolveu o maior problema das redes sociais: ter apenas um link na sua bio.
+                </p>
+                <p>
+                    Agilinks foi o primeiro plataforma de 'link na bio' criada para resolver esse problema
+                </p>
+                <p>
+                    Desde então Agilinks se tornou muito mais. Permitindo empresas e criadores a conseguir muito mais das suas redes socias,
+                    aumentando seus seguidores e devolvendo o controle de como seus conteúdos são descobertos.
+                </p>
+            </div>
 
         </div>
     </main>
@@ -64,7 +68,7 @@
 
         window.setInterval(function() {
             roll_shape()
-        }, 2000);
+        }, 2500);
         
     </script>
 </body>
