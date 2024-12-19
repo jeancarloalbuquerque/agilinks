096f927fb44e6fd02393a3bec8c47db4b302aeb2
Jean Carlo Albuquerque <jeancarlo1584@gmail.com>
2024-12-19 11:16:24 -0400

    collections index

---
+++ admin/collections/index.php
@@ -0,0 +1,73 @@
+<?php
+    require_once '../../vendor/autoload.php'; 
+    require_once '../../database/connection.php';
+
+    $collections = $mysqli->query("SELECT * FROM collections");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/fomantic-ui@2.9.3/dist/semantic.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/fomantic-ui@2.9.3/dist/semantic.min.js"></script>
+    
+    <title>Coleções</title>
+</head>
+<body>
+    <?php include_once '../../resources/views/layouts/navigation/auth.php'; ?>
+
+    <div class="ui container">
+        <h1 class="ui header">
+            <a href="./create.php" class="ui right floated primary button">Novo</a>
+            Coleções
+        </h1>
+
+        <table class="ui table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nome</th>
+                    <th></th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php foreach ($collections as $collection) {
+                    $id = $collection['id'];    
+                    $name = $collection['name'];    
+                ?>
+                    <tr>
+                        <td><?= $id ?></td>
+                        <td><?= $name ?></td>
+                        <td>
+                            <div class="ui compact icon buttons">
+                                <a href="./edit?id=<?= $id ?>" class="ui button">
+                                    <i class="pencil icon"></i>
+                                </a>
+
+                                <form action="./delete" method="post" id="delete-form-<?= $id ?>" style="display: none">
+                                    <input type="hidden" name="id" value="<?= $id ?>">
+                                </form>
+
+                                <button class="ui button">
+                                    <i class="trash icon"></i>
+                                </button>
+
+                            </div>
+
+                        </td>
+                    </tr>
+
+                <?php } ?>
+
+            </tbody>
+        </table>
+
+    </div>
+</body>
+</html>
