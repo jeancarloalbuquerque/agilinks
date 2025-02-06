a39da6a1781a378b212a79e902cc9aad48cfddd7
Jean Carlo Albuquerque <jeancarlo1584@gmail.com>
2025-02-06 14:28:36 -0400

    adding user_id to store data

--- admin/collections/store.php
+++ admin/collections/store.php
@@ -2,10 +2,13 @@
     require_once '../../vendor/autoload.php'; 
     require_once '../../database/connection.php';
 
+    session_start();
+
     if (!empty($_POST)) {
         $name = $_POST['name'];
+        $user_id = $_SESSION['user']['id'];
 
-        $query = "INSERT INTO collections (name) VALUES ('$name')";
+        $query = "INSERT INTO collections (name, user_id) VALUES ('$name', $user_id)";
 
         if ($mysqli->query($query)) {
             header('Location: ./index.php');
--- admin/links/store.php
+++ admin/links/store.php
@@ -2,13 +2,16 @@
     include_once '../../vendor/autoload.php';
     include_once '../../database/connection.php';
 
+    session_start();
+
     if (!empty($_POST)) {
         $title = $_POST['title'];
         $description = $_POST['description'];
         $url = $_POST['url'];
         $collection = !empty($_POST['collection']) ? $_POST['collection'] : 'null';
+        $user_id = $_SESSION['user']['id'];
         
-        $query = "INSERT INTO links (title, description, url, collection_id) VALUES ('$title', '$description', '$url', $collection)";
+        $query = "INSERT INTO links (title, description, url, collection_id, user_id) VALUES ('$title', '$description', '$url', $collection, $user_id)";
 
         if ($mysqli->query($query)) {
             header('Location: ./');
