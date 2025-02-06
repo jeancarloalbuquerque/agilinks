ad50071cc55f3dfc7b2e01fc2d6040dad8d4daf5
Jean Carlo Albuquerque <jeancarlo1584@gmail.com>
2025-02-06 17:02:11 -0400

    update user page

--- users/index.php
+++ users/index.php
@@ -2,12 +2,30 @@
     require_once '../vendor/autoload.php'; 
     require_once '../database/connection.php';
 
-    $collections = $mysqli->query('SELECT * FROM collections ORDER BY name')->fetch_all(MYSQLI_ASSOC);
+    function back()
+    {
+        return header('Location: /');
+    }
+
+    if (empty($_GET['user'])) {
+        return back();
+    }
+
+    $username = $_GET['user'];
+    $user = $mysqli->query("SELECT * FROM users WHERE username = '$username'");
+
+    if ($user->num_rows !== 1) {
+        return back();
+    }
+
+    $user = $user->fetch_assoc();
+    $user_id = $user['id'];
 
     $links = $mysqli->query(
-        'SELECT links.*, collections.name as collection_name  FROM links 
+        "SELECT links.*, collections.name as collection_name  FROM links 
         LEFT JOIN collections ON links.collection_id = collections.id
-        ORDER BY collections.name, title'
+        WHERE links.user_id = '$user_id'
+        ORDER BY collections.name, title"
     );
 ?>
 
@@ -28,7 +46,12 @@
         <div class="ui basic center aligned segment">
             <img class="ui small circular centered image" src="https://images.unsplash.com/photo-1734375119887-460f4b97dfaa?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" style="aspect-ratio:1; object-fit:cover">
             
-            <h1 class="ui header">@Asperiores Provident</h1>
+            <h1 class="ui header">
+                @<?= $user['username'] ?>
+                <div class="sub header">
+                    <?= $user['name'] ?>
+                </div>
+            </h1>
 
             <a class="ui circular twitter icon button"><i class="twitter icon"></i></a>
             <a class="ui circular pink icon button"><i class="instagram icon"></i></a>
@@ -36,26 +59,28 @@
 
         </div>
 
+        <div class="ui hidden section divider"></div>
+
         <div class="ui basic center aligned segment">
-            <div class="ui divided relaxed link items">
+            <div class="ui relaxed link items">
                 <?php
                     $previous_collection_id = -1;
                     foreach ($links as $link) {
                 ?>
-                    <?php if ($link['collection_id'] != $previous_collection_id) {
-                        $previous_collection_id = $link['collection_id']; ?>
-
-                        <?php if (!empty($link['collection_name'])) { ?>
+                    <?php 
+                        if ($link['collection_id'] != $previous_collection_id) {
+                        $previous_collection_id = $link['collection_id'];
+                        
+                        if (!empty($link['collection_name'])) { 
+                        ?>
                             <div class="ui hidden section divider"></div>
                             <h1 class="ui header"><?= $link['collection_name'] ?></h1>
                         <?php } ?>
-                        
                     <?php } ?>
 
                     <a href="<?= $link['url'] ?>" class="item">
                         <div class="content">
                             <div class="header"><?= $link['title'] ?></div>
-                            <!-- <div class="description"><?= $link['description'] ?></div> -->
                         </div>
                     </a>
                 
