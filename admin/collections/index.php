4deb014acd22accd6b95ba0fcb1d23d32e7e14f1
Jean Carlo Albuquerque <jeancarlo1584@gmail.com>
2025-02-06 11:12:54 -0400

    add num links in collection index

--- admin/collections/index.php
+++ admin/collections/index.php
@@ -2,7 +2,14 @@
     require_once '../../vendor/autoload.php'; 
     require_once '../../database/connection.php';
 
-    $collections = $mysqli->query("SELECT * FROM collections ORDER BY name");
+    $collections = $mysqli->query(
+        "SELECT collections.*, count(links.collection_id) as links_count 
+        FROM collections 
+        LEFT JOIN links 
+        ON (collections.id = links.collection_id)
+        GROUP by collections.id
+        ORDER BY collections.name"
+    );
 
 ?>
 
@@ -27,11 +34,12 @@
             Coleções
         </h1>
 
-        <table class="ui table">
+        <table class="ui fluid table">
             <thead>
                 <tr>
                     <th>Id</th>
                     <th>Nome</th>
+                    <th>N. Links</th>
                     <th></th>
                 </tr>
             </thead>
@@ -39,28 +47,30 @@
             <tbody>
                 <?php foreach ($collections as $collection) {
                     $id = $collection['id'];    
-                    $name = $collection['name'];    
+                    $name = $collection['name'];
+                    $links_count = $collection['links_count'];
                 ?>
                     <tr>
                         <td><?= $id ?></td>
                         <td><?= $name ?></td>
+                        <td><?= $links_count ?></td>
                         <td>
                             <div class="ui compact icon buttons">
-                                <a href="./edit.php?id=<?= $id ?>" class="ui button">
-                                    <i class="pencil icon"></i>
+                                <a href="./edit.php?id=<?= $id ?>" class="ui basic tiny button">
+                                    <i class="blue pencil icon"></i>
                                 </a>
 
                                 <form action="./delete" method="post" id="delete-form-<?= $id ?>" style="display: none">
                                     <input type="hidden" name="id" value="<?= $id ?>">
                                 </form>
 
-                                <button class="ui button" 
+                                <button class="ui basic tiny button" 
                                     onclick="
                                         if (confirm('Deseja realmente deletar esse recurso?')) {
                                             document.getElementById('delete-form-<?= $id ?>').submit()
                                         }"
                                 >
-                                    <i class="trash icon"></i>
+                                    <i class="red trash icon"></i>
                                 </button>
 
                             </div>
