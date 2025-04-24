54b9df4d3a6ec9d046b037a86c327fcb539ee96d
Jean Carlo Albuquerque <jeancarlo1584@gmail.com>
2025-04-23 21:52:54 -0400

    create home view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@section('main')
+    <div class="ui text container">
+
+        <div class="ui center aligned basic segment">
+            <h1>Ajude seus seguidores a descobrir tudo que você faz, com apenas um link</h1>
+            <a href="./admin" class="ui primary huge button">Começar agora</a>
+        </div>
+
+        <div class="ui hidden section divider"></div>
+
+        <div class="ui basic segment">
+            <h2>
+                <span>Usado por mais de 50M de </span>
+                <div class="ui text shape" id="users-shape">
+                    <div class="sides">
+                        <div class="active side"><span class="ui primary text">Músicos</span></div>
+                        <div class="side"><span class="ui primary text">Bandas</span></div>
+                        <div class="side"><span class="ui primary text">Escritores</span></div>
+                        <div class="side"><span class="ui primary text">Criadores</span></div>
+                        <div class="side"><span class="ui primary text">Influenciadores</span></div>
+                        <div class="side"><span class="ui primary text">Pequenos Negócios</span></div>
+                        <div class="side"><span class="ui primary text">Atletas</span></div>
+                        <div class="side"><span class="ui primary text">Streemers</span></div>
+                        <div class="side"><span class="ui primary text">Vloogers</span></div>
+                    </div>
+                </div>
+            </h2>
+        </div>
+
+        <div class="ui section hidden divider"></div>
+
+        <div class="ui basic segment">
+            <h2>Você nunca mais vai precisar mudar o link da sua Bio!</h2>
+            <p class="ui text">
+                Em 2024, a Agilinks resolveu o maior problema das redes sociais: ter apenas um link na sua bio.
+            </p>
+            <p class="ui text">
+                A Agilinks foi a primeira plataforma de <span class="ui blue text">link na bio</span> criada para resolver esse problema.
+                Desde então Agilinks se tornou muito mais. Permitindo empresas e criadores a conseguir muito mais das suas redes socias,
+                aumentando seus seguidores e devolvendo o controle de como seus conteúdos são descobertos.
+            </p>
+        </div>
+
+    </div>
+
+    <script>
+        function roll_shape() {
+            $('.shape').shape('flip down');
+        }
+
+        window.setInterval(function() {
+            roll_shape()
+        }, 2500);
+        
+    </script>
+@endsection
