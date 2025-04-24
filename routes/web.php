49f03b0dbf400655103b9928ba0c8a80073e5e8c
Jean Carlo Albuquerque <jeancarlo1584@gmail.com>
2025-04-23 21:52:48 -0400

    add auth middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,18 @@
 use App\Http\Controllers\CollectionController;
 use App\Http\Controllers\LinkController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\Test;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('links.index');
+    return view('home');
 })->name('home');
 
-Route::resource('links', LinkController::class);
-Route::resource('collections', CollectionController::class);
+
 
 Route::middleware('auth')->group(function () {
+    Route::resource('links', LinkController::class);
+    Route::resource('collections', CollectionController::class);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
